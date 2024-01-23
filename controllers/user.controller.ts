48d1c8bd23b80c67dc1635159113109076339524
crazy0629 <milan.jansen0629@gmail.com>
2024-01-24 00:43:25 +0900

    add: telegram bot & user controller

---
+++ controllers/user.controller.ts
@@ -0,0 +1,56 @@
+import { deleteModel } from "mongoose";
+import UserInfo from "../models/user";
+
+export const saveUserId = async (chatId: number) => {
+  try {
+    const model = await UserInfo.findOne({ userId: chatId });
+    if (model !== null) {
+      const result = {
+        success: false,
+        message:
+          "User was already subscribed to Me! I will send you new update!",
+      };
+      return result;
+    }
+    const newUser = new UserInfo();
+    newUser.userId = chatId;
+    await newUser.save();
+    const result = {
+      success: true,
+      message:
+        "Thank you for your interest!, User is successfully subscribed to Me!",
+    };
+    return result;
+  } catch (error) {
+    const result = {
+      success: false,
+      message: "Oops! Error occured! Please try again!",
+    };
+    return result;
+  }
+};
+
+export const removeUserId = async (chatId: number) => {
+  try {
+    const model = await UserInfo.findOne({ userId: chatId });
+    if (model == null) {
+      const result = {
+        success: false,
+        message: "You had not subscribed to me! Thank you!",
+      };
+      return result;
+    }
+    const deletedModel = await UserInfo.deleteOne({ userId: chatId });
+    const result = {
+      success: true,
+      message: "Okay, no worries. We will stop sending you notification",
+    };
+    return result;
+  } catch (error) {
+    const result = {
+      success: false,
+      message: "Oops! Error occured! Please try again!",
+    };
+    return result;
+  }
+};
--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import http from "http";
 import app from "./app";
 import "./database";
+import { startBot } from "./service/bobot";
 
 const server = http.createServer(app);
 
@@ -8,4 +9,5 @@ const port = app.get("port") || 8000; // Ensure 'port' is set in 'app'
 
 server.listen(port, () => {
   console.log(`>> Server is running on port ${port}`);
+  startBot();
 });
